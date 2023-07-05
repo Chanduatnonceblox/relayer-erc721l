bc7eec2ad28fd54e11edba48f1bce9bf23d564ce
Chandu8817 <chandu@DESKTOP-9FSANNC>
2023-07-05 16:52:59 +0530

    first commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,56 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import { WagmiConfig, createConfig, configureChains } from 'wagmi'
+import { goerli, polygonMumbai } from '@wagmi/core/chains'
+ 
+
+import { publicProvider } from 'wagmi/providers/public'
+ 
+import { CoinbaseWalletConnector } from 'wagmi/connectors/coinbaseWallet'
+import { InjectedConnector } from 'wagmi/connectors/injected'
+import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
+import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
+ 
+// Configure chains & providers with the Alchemy provider.
+// Two popular providers are Alchemy (alchemy.com) and Infura (infura.io)
+const { chains, publicClient, webSocketPublicClient } = configureChains(
+  [goerli, polygonMumbai],
+  [ publicProvider()],
+)
+ 
+// Set up wagmi config
+const config = createConfig({
+  autoConnect: true,
+  connectors: [
+    new MetaMaskConnector({ chains }),
+    new CoinbaseWalletConnector({
+      chains,
+      options: {
+        appName: 'wagmi',
+      },
+    }),
+    new WalletConnectConnector({
+      chains,
+      options: {
+        projectId: 'beb477fc5d17fde4546fe0838460132b',
+      },
+    }),
+    new InjectedConnector({
+      chains,
+      options: {
+        name: 'Injected',
+        shimDisconnect: true,
+      },
+    }),
+  ],
+  publicClient,
+  webSocketPublicClient,
+})
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return  <WagmiConfig config={config}>
+
+    <Component {...pageProps} />
+</WagmiConfig>
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,347 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import {
+  useAccount,
+  useConnect,
+  useDisconnect,
+  useEnsName,
+  useNetwork,
+} from "wagmi";
+import { useEffect, useState } from "react";
+import { useSwitchNetwork } from "wagmi";
 
-const inter = Inter({ subsets: ['latin'] })
+import abi from "../utils/abi.json";
+import erc721Abi from "../utils/erc721abi.json";
+import networks from "../utils/xdapp.config.json";
+import { ethers } from "ethers";
+import {
+  getEmitterAddressEth,
+  parseSequenceFromLogEth,
+} from "@certusone/wormhole-sdk";
+declare let window: any;
+
+const inter = Inter({ subsets: ["latin"] });
+
+let provider: ethers.providers.Provider | ethers.Signer | undefined;
 
 export default function Home() {
+  const { address, connector, isConnected } = useAccount();
+  const { switchNetwork } = useSwitchNetwork();
+  const { data: ensName } = useEnsName({ address });
+  const { chain } = useNetwork();
+  const { connect, connectors, error, isLoading, pendingConnector, data } =
+    useConnect();
+  const { disconnect } = useDisconnect();
+
+  const [domLoaded, setDomLoaded] = useState(false);
+  const [isApporved, setIsApproved] = useState(false);
+  const [vaaBytes, setVaaBytes] = useState();
+
+  const [tokenAddress, setTokenAddress] = useState("");
+  const [tokenId, setTokenId] = useState("");
+  const [selectedChain, setSelectedChain] = useState();
+  const [destinationChain, setDestinationChain] = useState();
+
+  let signer = provider?.getSigner();
+
+  let bridgeAddress =
+    chain?.id == networks.networks.polygon.chainId
+      ? networks.networks.polygon.coreBridgeAddress
+      : networks.networks.ethereum.coreBridgeAddress;
+  let nftbridgeAddress =
+    chain?.id == networks.networks.polygon.chainId
+      ? networks.networks.polygon.bridgeAddress
+      : networks.networks.ethereum.bridgeAddress;
+
+  let NFTBridgeInstance = new ethers.Contract(nftbridgeAddress, abi, signer);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      provider = new ethers.providers.Web3Provider(window?.ethereum);
+      signer = provider?.getSigner();
+      NFTBridgeInstance = new ethers.Contract(nftbridgeAddress, abi, signer);
+    }
+    if (chain?.id != selectedChain) {
+      switchNetwork?.(selectedChain);
+    }
+    switchNetwork?.(destinationChain);
+    console.log(chain?.id);
+    setDomLoaded(true);
+    console.log(vaaBytes);
+  }, []);
+
+  const handleApprove = async () => {
+    try {
+      if (tokenAddress != undefined) {
+        provider = new ethers.providers.Web3Provider(window?.ethereum);
+        signer = provider?.getSigner();
+        const ERC721Instacne = new ethers.Contract(
+          tokenAddress,
+          erc721Abi,
+          signer
+        );
+
+        let isApprove = await ERC721Instacne.isApprovedForAll(
+          address,
+          nftbridgeAddress
+        );
+
+        if (!isApprove) {
+          let recp = await ERC721Instacne.approve(nftbridgeAddress, tokenId, {
+            from: address,
+          });
+          await recp.wait();
+          setIsApproved(true);
+        } else {
+          setIsApproved(true);
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const handleWithdrawal = async (_vaaBytes: any) => {
+    try {
+      let recpit = await NFTBridgeInstance.completeTransfer(
+        Buffer.from(_vaaBytes, "base64")
+      );
+      recpit = await recpit.wait();
+
+      console.log(recpit);
+      setVaaBytes(undefined);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleTransferNFT = async () => {
+    try {
+      let recipientChain = 2;
+
+      let nonce = Math.floor(4301559040 + Math.random() * 900000);
+      let bytecodeAddress;
+      if (address != undefined) {
+        bytecodeAddress = ethers.utils.keccak256(address);
+      }
+      let tx = await NFTBridgeInstance.transferNFT(
+        tokenAddress,
+        tokenId,
+        recipientChain,
+        bytecodeAddress,
+        "2301559040",
+        { from: address }
+      );
+      tx = await tx.wait();
+      console.log(tx);
+
+      const emitterAddr = getEmitterAddressEth(nftbridgeAddress);
+      const seq = parseSequenceFromLogEth(tx, bridgeAddress);
+      const vaaURL = `${networks.wormhole.restAddress}/v1/signed_vaa/${networks.networks.polygon.wormholeChainId}/${emitterAddr}/${seq}`;
+      let _vaaBytes = await (await fetch(vaaURL)).json();
+      while (!_vaaBytes.vaaBytes) {
+        console.log("VAA not found, retrying in 5s!");
+        await new Promise((r) => setTimeout(r, 5000)); //Timeout to let Guardiand pick up log and have VAA ready
+        _vaaBytes = await (await fetch(vaaURL)).json();
+        console.log(_vaaBytes.vaaBytes, "in");
+        setVaaBytes(_vaaBytes.vaaBytes);
+      }
+      setVaaBytes(_vaaBytes.vaaBytes);
+      setIsApproved(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleSwtichSourceNetwork = async (e: any) => {
+    console.log(destinationChain);
+
+    try {
+      
+      setSelectedChain(e.target.value);
+      setSelectedChain(e.target.value);
+      await window.ethereum.request({
+        method: "wallet_switchEthereumChain",
+        params: [{ chainId: ethers.utils.hexValue(e.target.value) }], // chainId must be in hexadecimal numbers
+      });
+      provider = new ethers.providers.Web3Provider(window?.ethereum);
+      signer = provider?.getSigner();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleSwtichDestinationNetwork = async (e: any) => {
+    setDestinationChain(e.target.value);
+  };
+
+  if (isConnected) {
+    return (
+      <main>
+        {domLoaded && (
+          <div className="mt-4">
+            <div>{ensName ? `${ensName} (${address})` : address}</div>
+            <div>Connected to {connector?.name}</div>
+            <button onClick={disconnect}>Disconnect</button>
+
+            <div className="max-w-md mx-auto">
+              <form className="w-96">
+                <div className="mb-4 grid grid-rows-1 grid-flow-col gap-4">
+                  <div>
+                    <label
+                      htmlFor="SourceChain"
+                      className="block text-gray-700 text-sm font-bold mb-2"
+                    >
+                      Source Chain
+                    </label>
+                    <div className="mt-2">
+                      <select
+                        id="SourceChain"
+                        name="sourceChain"
+                        autoComplete="source-chain"
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        onChange={handleSwtichSourceNetwork}
+                      >
+                        <option value={80001}>Polygon</option>
+                        <option value={5}>Ethereum</option>
+                      </select>
+                    </div>
+                  </div>
+                  <div>
+                    <label
+                      htmlFor="DestinationChain"
+                      className="block text-gray-700 text-sm font-bold mb-2"
+                    >
+                      Destination Chain
+                    </label>
+                    <div className="mt-2">
+                      <select
+                        id="DestinationChain"
+                        name="DestinationChain"
+                        autoComplete="destination-chain"
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        onChange={handleSwtichDestinationNetwork}
+                      >
+                        <option value={5}>Ethereum</option>
+                        <option value={80001}>Polygon</option>
+                      </select>
+                    </div>
+                  </div>
+                </div>
+                <div className="mb-4">
+                  <label
+                    htmlFor="input1"
+                    className="block text-gray-700 text-sm font-bold mb-2"
+                  >
+                    Token Address:
+                  </label>
+                  <input
+                    id="input1"
+                    type="text"
+                    value={tokenAddress}
+                    placeholder="0xD8E4C2DbDd2e2bd8F1336EA691dBFF6952B1a6eB"
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    onChange={(e) => setTokenAddress(e.target.value)}
+                  />
+                </div>
+                <div className="mb-4">
+                  <label
+                    htmlFor="input2"
+                    className="block text-gray-700 text-sm font-bold mb-2"
+                  >
+                    Token Id:
+                  </label>
+                  <input
+                    id="input2"
+                    type="text"
+                    value={tokenId}
+                    onChange={(e) => setTokenId(e.target.value)}
+                    placeholder="12345"
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  />
+                </div>
+                <div className="flex items-center justify-center">
+                  {isApporved ? (
+                    <button
+                      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                      type="button"
+                      onClick={handleTransferNFT}
+                    >
+                      Transfer
+                    </button>
+                  ) : vaaBytes ? (
+                    chain?.id == destinationChain ? (
+                      <button
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                        type="button"
+                        onClick={() => handleWithdrawal(vaaBytes)}
+                      >
+                        Withdraw
+                      </button>
+                    ) : (
+                      <button
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                        type="button"
+                        onClick={async (e) => {
+                          try {
+      
+                            setSelectedChain(destinationChain);
+                            setSelectedChain(destinationChain);
+                            await window.ethereum.request({
+                              method: "wallet_switchEthereumChain",
+                              params: [{ chainId: ethers.utils.hexValue(destinationChain) }], // chainId must be in hexadecimal numbers
+                            });
+                            provider = new ethers.providers.Web3Provider(window?.ethereum);
+                            signer = provider?.getSigner();
+                          } catch (error) {
+                            console.log(error);
+                          }
+                        }}
+                      >
+                        change network
+                      </button>
+                    )
+                  ) : (
+                    <button
+                      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                      type="button"
+                      onClick={(e) => {
+                        handleApprove();
+                      }}
+                    >
+                      Approve
+                    </button>
+                  )}
+                </div>
+              </form>
+            </div>
+          </div>
+        )}
+      </main>
+    );
+  }
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      {domLoaded && (
+        <div>
+          {connectors.map((connector) => (
+            <button
+              disabled={!connector.ready}
+              key={connector.id}
+              onClick={() => connect({ connector })}
+            >
+              {connector.name}
+              {!connector.ready && " (unsupported)"}
+              {isLoading &&
+                connector.id === pendingConnector?.id &&
+                " (connecting)"}
+            </button>
+          ))}
+
+          {error && <div>{error.message}</div>}
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      )}
     </main>
-  )
+  );
 }
